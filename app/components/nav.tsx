8d1ac617a480ac3732328fd80d76d6b03242baa8
Juan Morais <timrodz@icloud.com>
2024-07-15 22:17:14 +1200

    style: Colors

--- app/components/nav.tsx
+++ app/components/nav.tsx
@@ -17,7 +17,7 @@ export function Navbar() {
     <aside className="-ml-[8px] mb-16 tracking-tight">
       <div className="lg:sticky lg:top-20">
         <nav
-          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative text-neutral-600"
+          className="flex flex-row items-start relative px-0 pb-0 fade md:overflow-auto scroll-pr-6 md:relative text-neutral-600 dark:text-neutral-300"
           id="nav"
         >
           <div className="flex flex-row space-x-0 pr-10">
@@ -26,7 +26,7 @@ export function Navbar() {
                 <Link
                   key={path}
                   href={path}
-                  className="hover:underline transition-all hover:text-neutral-800 dark:hover:text-neutral-200 flex align-middle relative py-1 px-2 m-1 font-mono"
+                  className="hover:underline transition-all hover:text-neutral-800 dark:hover:text-neutral-100 flex align-middle relative py-1 px-2 m-1 font-mono"
                 >
                   {name}
                 </Link>
